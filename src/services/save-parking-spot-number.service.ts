05b91c2bd802a5c315740f0b2c7adcf33303b1af
JoaoPaulo-creator <joaosarto@gmail.com>
2023-02-11 23:36:44 -0300

    refactor: Removido controller que criava numero do spot

--- src/services/save-parking-spot-number.service.ts
+++
@@ -1,8 +0,0 @@
-import { store } from "../repository/create-parkingspot-number";
-
-export async function saveParkingSpotNumberService(
-  number: number,
-  isParkingSpotAvailable: boolean
-) {
-  return await store(number, isParkingSpotAvailable);
-}
