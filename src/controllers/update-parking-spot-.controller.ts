0cb50d73b5cda5014178abc91231d8a486a5b99b
JoaoPaulo-creator <joaosarto@gmail.com>
2023-02-13 23:11:17 -0300

    fix: Corrigido tratamento de erro no update

--- src/controllers/update-parking-spot-.controller.ts
+++ src/controllers/update-parking-spot-.controller.ts
@@ -1,4 +1,5 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
 import { updateParkingSpot } from "../services/update-paking-spot.service";
 
 export async function updateParkingSpotController(
@@ -7,14 +8,26 @@ export async function updateParkingSpotController(
 ) {
   //
 
-  const { id }: any = request.params;
-  const { apartment }: any = request.body;
+  const requetBody = z.object({
+    apartment: z
+      .string()
+      .min(1, "title is required")
+      .max(3, "title must be minor or equals to 3"),
+  });
 
-  await updateParkingSpot(id, apartment)
-    .then(() => {
-      return reply.code(200).send({ apartment });
-    })
-    .catch((err) => {
-      console.log(err);
+  try {
+    const { id }: any = request.params;
+    const { apartment } = requetBody.parse(request.body);
+
+    const update = await updateParkingSpot(id, apartment).then(() => {
+      return { apartment };
     });
+
+    return update;
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const { message } = error;
+      return reply.code(400).send({ message });
+    }
+  }
 }
